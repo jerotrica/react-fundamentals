fe7c259e8bb5c4b12cb10904c8cf8a846e9a51b9
Jeronimo <jerotrica@gmail.com>
2023-06-29 17:56:13 -0300

    exercise 6 completed

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -10,18 +10,15 @@ function UsernameForm({ onSubmitUsername }) {
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
   //
-
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
     onSubmitUsername(event.target.elements.usernameInput.value)
   }
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
-
   // 🐨 add the onSubmit handler to the <form> below
-
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
